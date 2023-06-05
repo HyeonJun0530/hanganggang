23f5d68d18831168b14f6337bfa45b44eae6d6ec
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-06-05 18:45:34 +0900

    feat: Raspi 통신을 위한 Dto 구현

---
+++ src/main/java/com/upbit/hanganggang/dto/CoinRaspiResponseDto.java
@@ -0,0 +1,10 @@
+package com.upbit.hanganggang.dto;
+
+import lombok.Data;
+
+@Data
+public class CoinRaspiResponseDto {
+    private String market;
+    private String tradePrice;
+    private String change;
+}
