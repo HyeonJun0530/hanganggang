a304906379b3a7c42036e021d447dcbad0d37894
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-06-05 18:24:20 +0900

    feat: CoinController 구현

--- src/main/java/com/upbit/hanganggang/controller/CoinController.java
+++ src/main/java/com/upbit/hanganggang/controller/CoinController.java
@@ -1,8 +1,9 @@
 package com.upbit.hanganggang.controller;
 
+import com.upbit.hanganggang.constant.ApiUrlEnum;
 import com.upbit.hanganggang.dto.CoinPriceResponseDto;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import com.upbit.hanganggang.service.CoinService;
+import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -11,42 +12,42 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
-import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+@RequiredArgsConstructor
 @Controller
 public class CoinController {
-    @GetMapping("/btc-price")
+
+    private final CoinService coinService;
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    @GetMapping("/coin-price")
     public String getCoinPrice(Model model) throws ParseException {
 
-        JSONParser jsonParser = new JSONParser();
-        DecimalFormat decimalFormat = new DecimalFormat("#,###");
-        // API 요청을 보낼 URL
-        String apiUrl = "https://api.upbit.com/v1/ticker?markets=KRW-BTC";
+        List<String> jsonCoinList = new ArrayList<>();
 
-        // RestTemplate을 사용하여 API 호출
-        RestTemplate restTemplate = new RestTemplate();
+        List<ApiUrlEnum> apiUrlEnumList = List.of(ApiUrlEnum.values());
 
-        RequestEntity<Void> req = RequestEntity
-                .get(apiUrl)
-                .build();
+        for (int i = 0; i < apiUrlEnumList.size(); i++) {
+            String apiUrl = "https://api.upbit.com/v1/ticker?markets=" + apiUrlEnumList.get(i).getToken();
+            RequestEntity<Void> req = RequestEntity
+                    .get(apiUrl)
+                    .build();
 
-        ResponseEntity<String> result = restTemplate.exchange(req,String.class);
-        String str = result.getBody();
-        str = str.replace("[", "").replace("]", "");
+            ResponseEntity<String> result = restTemplate.exchange(req,String.class);
+            String coin = result.getBody();
+            jsonCoinList.add(coin);
+        }
 
-        JSONObject jsonObject = (JSONObject) jsonParser.parse(str);
 
-        String market = jsonObject.get("market").toString();
-        Double tradePrice = (Double) jsonObject.get("trade_price");
-        String change = jsonObject.get("change").toString();
+        Map<Integer, Object> coinMap = coinService.createCoinList(jsonCoinList);
+        List<CoinPriceResponseDto> coinList = (List<CoinPriceResponseDto>) coinMap.get(0);
+        Integer count = (Integer) coinMap.get(1);
+        model.addAttribute("coinList",coinList);
+        model.addAttribute("count", count);
 
-        CoinPriceResponseDto coinPriceResponseDto = new CoinPriceResponseDto();
-        coinPriceResponseDto.setMarket(market);
-        coinPriceResponseDto.setTradePrice(decimalFormat.format(tradePrice));
-        coinPriceResponseDto.setChange(change);
-
-        model.addAttribute("coin",coinPriceResponseDto);
-
-        return "btckrw";
+        return "coin";
     }
 }
